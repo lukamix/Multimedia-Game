82fa67d05e94df35f922f5c9e9dab6b837a9ff23
NguyenDucThangNghiemTuc <thang.nd182629@gmail.com>
2021-12-27 17:47:27 +0700

    update

---
+++ src/Assets/Scripts/Transfer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transfer : MonoBehaviour
+{
+    // Handle gameobjects collider with a Victory object
+	void OnCollisionEnter2D (Collision2D other) {
+		if (other.gameObject.tag == "Player")
+		{
+            new WaitForSeconds(2.0f);
+			// if player then send it to the secret place
+			other.gameObject.GetComponent<Transform>().position = new Vector3(-2,-12,0);
+		} 
+	}
+}
